b7cd001f93a6a31461a7edde27f3bf5b9e762d2f
adkelino591 <129129230+adkelino591@users.noreply.github.com>
2023-09-04 19:52:51 -0600

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -18,3 +18,42 @@ console.log("word in LowerCase = " + word);
 
 
 }
+question_turn="player1";
+answer_turn="player2";
+function check()
+{
+    get_answer= document.getElementById("imput_check_box").value;
+    answer=get_answer.toLowerCase();
+    console.log("answer in lower case - " + answer);
+    if(answer_turn == word)
+    {
+if(answer_turn == "player1")
+{
+    player1_score=player1_score+1;
+    document.getElementById("player1_score").innerHTML=player1_score;
+}
+else
+{
+    player2_score=player2_score+1;
+    document.getElementById("player2_score").innerHTML=player2_score;
+}
+}
+if(question_turn =="player1")
+{question_turn ="player2"
+document.getElementById("player_question").innerHTML="turno para preguntar - " + player2_name;
+else
+{question_turn ="player1"
+document.getElementById("player_question").innerHTML="turno para preguntar - " + player1_name;}
+}
+if
+(answer_turn =="player1")
+{
+answer_turn ="player2"
+document.getElementById("player_answer").innerhtml="turno para responder - " + player2_name
+}else
+{
+    answer_turn ="player2"
+    document.getElementById("player_answer").innerhtml="turno para responder - " + player2_name
+    }
+document.getElementById("output").innerHTML=""
+}
